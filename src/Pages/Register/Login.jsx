f6d064bb58c3b8a8344a3d11d06b685f6ad1b14a
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-09-13 19:17:14 +0430

    refactor: login with redux

--- src/Pages/Register/Login.jsx
+++ src/Pages/Register/Login.jsx
@@ -1,73 +1,67 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getCurrentUser, loginUser } from "../../Services/courseServices";
-import { useDispatch } from "react-redux";
-import { authActions } from "../../store/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { loginFetch } from "../../store/authSlice";
+import CenterLoader from "../../Components/Loding/CenterLoader";
 
 const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    const [errors, setErrors] = useState({})
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    const handleLoginForm = async (e) => {
+    const handleLoginForm = (e) => {
         e.preventDefault()
-
-        try {
-            const { status, data } = await loginUser({ username, password })
-            if (status === 200) {
-                localStorage.setItem("token", data.key)
-                const loggedInUser = await getCurrentUser(data.key)
-                dispatch(authActions.login(loggedInUser.data))
-                navigate('/')
-            }
-        } catch (err) {
-            setErrors(err.response.data)
-        }
+        const data = { username, password }
+        dispatch(loginFetch({ data, navigate }))
     }
 
+    const { loading, errors } = useSelector((state) => state.auth)
+
     return (
-        <div className="container my-5">
-            <div className="row justify-content-center">
-                <div className="col-md-4 bg-white p-3 rounded-3 border text-center">
-                    <h2 className="fw-bold">ورود به حساب</h2>
-                    <form onSubmit={handleLoginForm} autoComplete="off">
-                        <div className="row">
-                            <div className="col-md-12 my-2">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    onChange={e => setUsername(e.target.value)}
-                                    placeholder="نام کاربری"
-                                />
+        <>
+            {loading && <CenterLoader />}
+            <div className="container my-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-4 bg-white p-3 rounded-3 border text-center">
+                        <h4 className="fw-bold">ورود به حساب</h4>
+                        <form onSubmit={handleLoginForm} autoComplete="off">
+                            <div className="row">
+                                <div className="col-md-12 my-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        onChange={e => setUsername(e.target.value)}
+                                        placeholder="نام کاربری"
+                                    />
+                                </div>
+                                <div className="col-md-12 mb-1">
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        onChange={e => setPassword(e.target.value)}
+                                        placeholder="رمز عبور"
+                                    />
+                                    {
+                                        errors ? (
+                                            <ul>
+                                                {errors.non_field_errors?.map((error, index) => (
+                                                    <li key={index} className="text-danger my-1">{error}</li>
+                                                ))}
+                                            </ul>
+                                        ) : undefined
+                                    }
+                                </div>
+                                <div className="col-md-12">
+                                    <button type="submit" className="btn btn-outline-success w-100">ورود</button>
+                                </div>
                             </div>
-                            <div className="col-md-12 mb-1">
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    onChange={e => setPassword(e.target.value)}
-                                    placeholder="رمز عبور"
-                                />
-                                {
-                                    errors ? (
-                                        <ul>
-                                            {errors.non_field_errors?.map((error, index) => (
-                                                <li key={index} className="text-danger">{error}</li>
-                                            ))}
-                                        </ul>
-                                    ) : undefined
-                                }
-                            </div>
-                            <div className="col-md-12">
-                                <button type="submit" className="btn btn-outline-success w-100">ورود</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,18 +1,46 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getCurrentUser, loginUser } from "../Services/courseServices";
+
+export const loginFetch = createAsyncThunk(
+  "auth/loginFetch",
+  async ({ data, navigate }, { dispatch }) => {
+    try {
+      const response = await loginUser(data);
+      if (response.status === 200) {
+        localStorage.setItem("token", response?.data?.key);
+        const isLoggedInUser = await getCurrentUser(response?.data?.key);
+        dispatch(authActions.login(isLoggedInUser?.data));
+        navigate("/courses");
+      }
+      return response;
+    } catch (err) {
+      return { errors: err?.response?.data };
+    }
+  }
+);
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState: {isLoggedIn: false, user: {}},
-    reducers: {
-        login(state, action) {
-            state.isLoggedIn = true
-            state.user = action.payload
-        },
-        logout(state) {
-            state.isLoggedIn = false
-            state.user = {}
-        }
-    }
-})
+  name: "auth",
+  initialState: { isLoggedIn: false, user: {}, loading: false, errors: [] },
+  reducers: {
+    login(state, action) {
+      state.isLoggedIn = true;
+      state.user = action.payload;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+      state.user = {};
+    },
+  },
+  extraReducers: {
+    [loginFetch.pending]: (state) => {
+      state.loading = true;
+    },
+    [loginFetch.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.errors = action?.payload?.errors;
+    },
+  },
+});
 
 export const authActions = authSlice.actions;
